ad7847d85a061c303737cdeac9a6407aa1644918
Jozef Cipa <cipa.jozef@gmail.com>
2023-07-17 00:25:05 +0200

    feat: merge frontends

---
+++ client/README.md
@@ -0,0 +1,3 @@
+This works as a redirecter to the washing machine control website.
+Its purpose is to prevent having an empty screen on the mobile webapp, when connected to a different network where the website is not accessible.
+Instead, this website will be loaded on `laundry.iot.jozefcipa.com` and if it finds local IP address of the device, it will redirect there, otheriwse it will just show an error message.
--- client/main.js
+++ client/main.js
@@ -1,7 +1,36 @@
-(async () => {
-  
+// TODO: add a specific URL endpoint that will return something in order to avoid false positives in case
+// 192.168.0.100 is used on a different network
+const URL = 'http://192.168.0.1'
+
+;(async () => {
+  // cancel the request after 1s to avoid endless waiting
+  const controller = new AbortController()
+  const loadingScreen = document.getElementById('loading')
+  const errorScreen = document.getElementById('error')
+  const laundryScreen = document.getElementById('laundry')
+  const status = document.getElementById('status')
+
+  try { 
+    // check if the web is accessible
+    setTimeout(() => controller.abort(), 1000)
+    await fetch(URL, { signal: controller.signal })
+    
+    // show laundry screen
+    loadingScreen.style.display = 'none'
+    laundryScreen.style.display = 'flex'
+    status.innerHTML = 'Washing...'
+  } catch {
+    // show error message
+    loadingScreen.style.display = 'none'
+    errorScreen.style.display = 'block'
+    status.innerHTML = `
+    Device not found on the network 🫣<br><br>
+    <small>Maybe you're on connected to a different network? Check your Wi-FI connection and try again.</small>
+    `
+  }
 
 
+  // TODO: other logic here
 
   // if done
   // add class .done to body, show washing-done
