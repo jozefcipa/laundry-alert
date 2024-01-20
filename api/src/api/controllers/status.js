0eb430a36ae3eb11819002ce8a5f8d311f75b900
Jozef Cipa <cipa.jozef@gmail.com>
2024-01-20 21:50:28 +0100

    feat(api): add uptime to /status

--- api/src/api/controllers/status.js
+++ api/src/api/controllers/status.js
@@ -1,9 +1,17 @@
+const os = require('os')
 const state = require('../../state')
+
+const apiStartTime = new Date()
+
+const systemStartTime = new Date()
+systemStartTime.setSeconds(systemStartTime.getSeconds() - os.uptime()) // current date - seconds since booting up
 
 async function status(req, res) {
   res.json({
     name: 'laundry-alert-api',
     isWashing: state.isWashing,
+    apiStartTime,
+    systemStartTime,
   })
 }
 
