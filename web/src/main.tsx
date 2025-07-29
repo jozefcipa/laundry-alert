b70dfcabc19a90d5d75ff336553af0f902807777
Jozef Cipa <cipa.jozef@gmail.com>
2025-07-29 15:40:10 +0200

    feat: refresh web on focus

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,4 +2,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { LaundryAlert } from './components/LaundryAlert.tsx'
 
+document.addEventListener('visibilitychange', () => {
+  if (document.visibilityState === 'visible') {
+    location.reload()
+  }
+})
+
 createRoot(document.getElementById('root')!).render(<LaundryAlert />)
