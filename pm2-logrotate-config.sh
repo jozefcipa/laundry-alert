bffe0a1593147029b8b1feccc80d9044467d2be7
Jozef Cipa <cipa.jozef@gmail.com>
2025-07-29 15:48:27 +0200

    feat: increase pm2 log size to 500MB

--- pm2-logrotate-config.sh
+++ pm2-logrotate-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pm2 set pm2-logrotate:max_size 10MB
+pm2 set pm2-logrotate:max_size 500MB
 
 # Run log rotate every 10 minutes,
 # as the app is not running for too long (max several hours)
